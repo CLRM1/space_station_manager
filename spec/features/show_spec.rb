f5e941e24b862ea36ff3aee98df6944f368bd6e3
Chris Romero <chrisleeromero1@gmail.com>
2022-03-23 01:55:07 -0600

    test: add first feature test to display the names of each space_station

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe 'space_station show page', type: :feature do
 # Then I see the name of each parent record in the system
 
   it 'displays the names of each space_station' do
-
+    station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
+    visit "/space_stations#{station.id}"
+    expect(page).to have_content(station.name)
+    # expect(page).to_not have_content(station_2.name)
   end
 end
