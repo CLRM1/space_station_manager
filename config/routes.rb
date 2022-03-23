5aac0bb9c3c74253e6b289467abf18fd6fb9450d
Chris Romero <chrisleeromero1@gmail.com>
2022-03-23 02:16:58 -0600

    feat: add index and show html files and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/space_stations', to: 'space_station#index'
+  get '/space_stations/:id', to: 'space_station#show'
 end
--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe 'space_station show page', type: :feature do
 
   it 'displays the names of each space_station' do
     station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
-    visit "/space_stations#{station.id}"
+    visit "/space_stations/#{station.id}"
+    save_and_open
     expect(page).to have_content(station.name)
     # expect(page).to_not have_content(station_2.name)
   end
