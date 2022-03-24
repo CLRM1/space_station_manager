5d1717750afefc43dc4f2e7085df6fa285c83463
Chris Romero <chrisleeromero1@gmail.com>
2022-03-23 23:33:14 -0600

    test: add test for second user story and initial attempt at displaying attributes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # README
 
-The data base schema is composed of two tables, space_stations and astronauts. 
+The data base schema is composed of two tables, SpaceStation and Astronauts. Where the primary key `id` for the SpaceStation table is used as the foreign key `station_id` in the Astronauts table to relate an Astronaut to its SpaceStation.
+
+ <img width="406" alt="space_station_database_schema" src="https://user-images.githubusercontent.com/26349568/159846094-12697a1e-7857-4b4e-93ed-41b0b11c82a6.png">
--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'space_station show page', type: :feature do
 
-#   [ ] done
+#   [x] done
 #
 # User Story 1, Parent Index
 #
@@ -18,5 +18,23 @@ RSpec.describe 'space_station show page', type: :feature do
     save_and_open_page
     expect(page).to have_content(station.name)
     expect(page).to have_content(station_2.name)
+  end
+
+#   [ ] done
+#
+# User Story 2, Parent Show
+#
+# As a visitor
+# When I visit '/parents/:id'
+# Then I see the parent with that id including the parent's attributes:
+# - data from each column that is on the parent table
+  it 'displays the attributes of a specific space station' do
+    station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
+    station_2 = SpaceStation.create!(name: 'ESA', habitable: false, max_occupants: 4)
+    visit "/space_stations/#{station.id}"
+    save_and_open_page
+    expect(page).to have_content(station.name)
+    expect(page).to have_content(station.habitable)
+    expect(page).to have_content(station.max_occupants)
   end
 end
