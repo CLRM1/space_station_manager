bf15bf971ba2b4cf529d624c1b2742acc8633798
Chris Romero <chrisleeromero1@gmail.com>
2022-03-24 21:46:46 -0600

    test: add test for user story 3

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -1,40 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'space_station show page', type: :feature do
-
-#   [x] done
-#
-# User Story 1, Parent Index
-#
-# For each parent table
-# As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
-
-  it 'displays the names of each space_station' do
-    station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
-    station_2 = SpaceStation.create!(name: 'ESA', habitable: false, max_occupants: 4)
-    visit "/space_stations"
-    save_and_open_page
-    expect(page).to have_content(station.name)
-    expect(page).to have_content(station_2.name)
+  before(:each) do
+    @station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
+    @station_2 = SpaceStation.create!(name: 'ESA', habitable: false, max_occupants: 4)
+    @walker = Astronaut.create!(name: 'Shanon Walker', active: true, years_active: 10, space_station_id: @station.id)
+    @kelly = Astronaut.create!(name: 'Scott Kelly', active: false, years_active: 7, space_station_id: @station.id)
   end
 
-#   [ ] done
-#
-# User Story 2, Parent Show
-#
-# As a visitor
-# When I visit '/parents/:id'
-# Then I see the parent with that id including the parent's attributes:
-# - data from each column that is on the parent table
+  it 'displays the names of each space_station' do
+    visit "/space_stations"
+    expect(page).to have_content(@station.name)
+    expect(page).to have_content(@station_2.name)
+  end
+
   it 'displays the attributes of a specific space station' do
-    station = SpaceStation.create!(name: 'ISS', habitable: true, max_occupants: 7)
-    station_2 = SpaceStation.create!(name: 'ESA', habitable: false, max_occupants: 4)
-    visit "/space_stations/#{station.id}"
-    save_and_open_page
-    expect(page).to have_content(station.name)
-    expect(page).to have_content(station.habitable)
-    expect(page).to have_content(station.max_occupants)
+    visit "/space_stations/#{@station.id}"
+    expect(page).to have_content(@station.name)
+    expect(page).to have_content(@station.habitable)
+    expect(page).to have_content(@station.max_occupants)
+  end
+
+  # [ ] done
+  #
+  # User Story 3, Child Index
+  #
+  # As a visitor
+  # When I visit '/child_table_name'
+  # Then I see each Child in the system including the Child's attributes:
+  it 'displays each astronaut in the system including the astronauts attributes' do
+    visit "/astronauts"
+    expect(page).to have_content(@walker.name)
   end
 end
