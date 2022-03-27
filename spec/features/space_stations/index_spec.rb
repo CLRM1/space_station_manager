d7535d4722a72accd17f11ac98616ca1d7105124
Chris Romero <chrisleeromero1@gmail.com>
2022-03-26 21:51:53 -0600

    feat: add link to space station index

--- spec/features/space_stations/index_spec.rb
+++ spec/features/space_stations/index_spec.rb
@@ -23,13 +23,26 @@ RSpec.describe 'space_station show page', type: :feature do
     expect(page).to have_content(@station_2.created_at)
   end
 
-    it 'displays a link at the top of the page that takes me to the astronauts index' do
-      visit "/space_stations"
-      expect(page).to have_link('Astronaut Index')
-      click_link 'Astronaut Index'
-      expect(current_path).to eq('/astronauts')
-      expect(current_path).to eq('/astronauts')
-      expect(page).to have_content(@walker.name)
-      expect(page).to have_content(@kelly.name)
-    end
+  it 'displays a link at the top of the page that takes me to the astronauts index' do
+    visit "/space_stations"
+    expect(page).to have_link('Astronaut Index')
+    click_link 'Astronaut Index'
+    expect(current_path).to eq('/astronauts')
+    expect(current_path).to eq('/astronauts')
+    expect(page).to have_content(@walker.name)
+    expect(page).to have_content(@kelly.name)
+  end
+#   User Story 9, Parent Index Link
+#
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Parent Index
+  it 'displays a link at the top of the page that takes me to the space stations index' do
+    visit "/space_stations"
+    expect(page).to have_link('Space Station Index')
+    click_link 'Space Station Index'
+    expect(current_path).to eq('/space_stations')
+    expect(page).to have_content(@station.name)
+    expect(page).to have_content(@station_2.name)
+  end
 end
