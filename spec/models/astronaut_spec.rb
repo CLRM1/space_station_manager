b8f340678d617d9e73aa4c79cd4dba06d55a19b1
Chris Romero <chrisleeromero1@gmail.com>
2022-03-29 15:55:56 -0600

    refactor: add class method tests for astronaut model

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Astronaut, type: :model do
     it { should allow_value(false).for(:active) }
   end
 
+  describe 'class methods' do
+    before(:each) do
+      @station = SpaceStation.create(name: 'Island Space Station', habitable: true, max_occupants: 10)
+      @astronaut_1 = Astronaut.create!(name: 'Kalpana Chawla', active: true, years_active: 5,  space_station_id: @station.id)
+      @astronaut_2 = Astronaut.create!(name: 'Jessica Meir', active: true, years_active: 5,  space_station_id: @station.id)
+      @astronaut_3 = Astronaut.create!(name: 'Peggy Whitson', active: false, years_active: 5,  space_station_id: @station.id)
+    end
+
+    it 'sorts the astronauts alphabetically' do
+      expect(Astronaut.sorted_astronauts).to eq([@astronaut_2, @astronaut_1, @astronaut_3])
+    end
+
+    it 'only displays astronauts that have true for the active attribute' do
+      expect(Astronaut.active_astronauts).to eq([@astronaut_1, @astronaut_2])
+    end
+  end
+
 end
