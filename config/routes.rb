102b055814bbeaa8003fe4337472a51b5a3293a8
Chris Romero <chrisleeromero1@gmail.com>
2022-04-02 22:23:28 -0600

    refactor: add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
   get '/space_stations', to: 'space_station#index'
   get 'space_stations/new', to: 'space_station#new'
   get '/space_stations/:id', to: 'space_station#show'
